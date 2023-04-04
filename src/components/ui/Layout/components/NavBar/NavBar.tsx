16d6934c797910f7387595a001cb3342b1e7c6f5
Koval Andrey <avikovall@gmail.com>
2023-04-04 03:15:31 +0300

    feat - completed NavBar style

--- src/components/ui/Layout/components/NavBar/NavBar.tsx
+++ src/components/ui/Layout/components/NavBar/NavBar.tsx
@@ -7,29 +7,36 @@ import styles from "./styles.module.scss";
 export const NavBar = () => {
 	const [isOpen, setOpen] = useState(false);
 
-	const navbarClasses = cn(
-		styles["navbar"],
-		{[styles["navbar_open"]]: isOpen}
-	)
-
+	
 	const handelShowNavBar = () => {
 		setOpen(true)
 	}
 	const handelHideNavBar = () => {
 		setOpen(false)
 	}
+	
+	const navbarClasses = cn(
+		styles["navbar"],
+		{[styles["navbar_open"]]: isOpen},
+	)
 
 	return (
 		<div 
 			className={navbarClasses} 
 			onMouseEnter={handelShowNavBar} 
 			onMouseLeave={handelHideNavBar}
-		>
-			<img src={logo} className={styles["logo"]} alt="logo" />
-			<div>
-				<Link className={styles["link"]} to={`/`}>Tic-tac-toe</Link>
-				<Link className={styles["link"]} to={`test`}>Test</Link>
-			</div>
+		>	
+			<Link className={styles["navbar__link"]} to={`/`}>
+				<img 
+					src={logo} 
+					className={styles["navbar__link__img"]} 
+					alt="logo"
+				/>
+			</Link>
+			<Link className={styles["navbar__link"]} to={`test`}>Test</Link>
+			<Link className={styles["navbar__link"]} to={`test`}>Test</Link>
+			<Link className={styles["navbar__link"]} to={`test`}>Test</Link>
+			<Link className={styles["navbar__link"]} to={`test`}>Test</Link>
 		</div>
 	)
 }
